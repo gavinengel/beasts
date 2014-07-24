01df2eeff67dca4b99be815b6cef56a7bec759b0
Kevin Leary <ktleary@gmail.com>
2014-07-24 12:43:55 +0000

    adding beasts

---
+++ app/models/bear.js
@@ -0,0 +1,11 @@
+// app/models.js
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BearSchema = new Schema({
+	name: String
+});
+
+module.exports = mongoose.model('Bear', BearSchema);
+
