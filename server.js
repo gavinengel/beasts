49702378c49ecd02a62d869d364a789bb8191a9a
Kevin Leary <ktleary@gmail.com>
2014-07-24 21:08:22 +0000

    finished add, retrieve, update, delete

--- server.js
+++ server.js
@@ -89,31 +89,19 @@ router.route('/bears/:bear_id')
 		res.json({message: "Bear updated!" });
 		});
 	});
-	});
+	})
 
-/*
-	// update the bear with this id (accessed at PUT http://localhost:8080/api/bears/:bear_id)
-	.put(function(req, res) {
 
-		// use our bear model to find the bear we want
-		Bear.findById(req.params.bear_id, function(err, bear) {
 
-			if (err)
-				res.send(err);
+	.delete(function(req, res){
 
-			bear.name = req.body.name; 	// update the bears info
-
-			// save the bear
-			bear.save(function(err) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'Bear updated!' });
-			});
-
+		Bear.remove({
+			_id: req.params.bear_id
+		}, function (err, bear){
+			if (err) res.send(err);
+		res.json({message: "Bear removed."});
 		});
 	});
-*/
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
