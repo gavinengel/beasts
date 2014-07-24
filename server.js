fb5a550aa14019e40dd52843e19af54ff390e102
root <root@ip-172-31-26-130.us-west-2.compute.internal>
2014-07-24 12:42:16 +0000

    working with remote db

--- server.js
+++ server.js
@@ -6,6 +6,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
+var Bear = require('./app/models/bear');
 
 // configure app for bodyparser
 // will use for POST
@@ -17,12 +20,33 @@ var port = process.env.PORT || 8080;
 // ========================
 var router = express.Router();
 
+// middleware to use for all requests
+router.use(function(req, res, next){
+	//logging
+	console.log('Rallying is happening');
+});
+
 // test router to make sure everything is working correctly
 router.get('/', function(req, res){
 	res.json({ message: 'hooray! welcome to our api!' });
 });
 
 // more routes will go here
+
+// routes that end in beasts
+router.route('/bears')
+
+	// create a beast
+	.post(function(req, res){
+		var bear = new Bear();
+		bear.name = req.body.name;
+
+		bear.save(function(err){
+			if(err)
+				res.send(err);
+			res.json({ message:'Beast Created!' });
+		});
+	});
 
 
 // Register our routes
@@ -32,7 +56,6 @@ app.use('/api', router);
 // Start the server
 app.listen(port);
 console.log('Rallying on port ' + port);
-
 
 
 
